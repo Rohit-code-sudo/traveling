f0bbc73a6fd3fb052bd4f96b220a7bfaf117538a
Rohit patel <114286436+Rohit-code-sudo@users.noreply.github.com>
2023-08-07 11:58:31 +0530

    Add files via upload
    ok

---
+++ trip/index.js
@@ -0,0 +1,41 @@
+
+//scroll reveal------------------------->
+
+ScrollReveal().reveal('#landingText', { delay: 500 , distance: '0px' , duration: 300 , easing:'ease-in-out',origin: 'top', });
+ScrollReveal().reveal('#c1,#c2,#c3', { delay: 100 , distance: '50px' , duration: 900 , easing:'ease-in-out',origin:'left' });
+ScrollReveal().reveal('.counter p',{ delay: 100 , distance: '50px' , duration: 900 , easing:'ease-in-out',origin:'right' ,scale:"0.85"});
+ScrollReveal().reveal('.counter a',{ delay: 100 , distance: '100px' , duration: 900 , easing:'ease-in',origin:'right'});
+ScrollReveal().reveal('#aboutText',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'top',})
+ScrollReveal().reveal('#aboutTextImg1',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'left',})
+ScrollReveal().reveal('#aboutTextImg2',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'right',})
+ScrollReveal().reveal('#parallex1 img',{ delay:150,distance:"50px",duration:900,easing:'ease-in-out',origin:'right',})
+ScrollReveal().reveal('#parallex1 h1',{ delay:150,distance:"50px",duration:900,easing:'ease-in-out',origin:'left',})
+
+ScrollReveal().reveal('#programPart1',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'top',})
+ScrollReveal().reveal('#programPart2 .bg-img',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'left',})
+ScrollReveal().reveal('#programPart2 h1,#programPart2 p,#programPart2 button',{ delay:150,distance:"30px",duration:600,easing:'ease-in-out',origin:'right',})
+ScrollReveal().reveal('#eventP1 h1',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'left',})
+ScrollReveal().reveal('#eventP1 button',{ delay:150,distance:"30px",duration:900,easing:'ease-in-out',origin:'top',})
+ScrollReveal().reveal('.eve-tick',{ duration:3000,move:0})
+ScrollReveal().reveal('#sponsor p',{ delay:150,distance:"60px",duration:900,easing:'ease-in-out',origin:'left',})
+ScrollReveal().reveal('#sponsor h1',{ delay:150,distance:"60px",duration:900,easing:'ease-in-out',origin:'right',})
+
+ScrollReveal().reveal('#sponsor img',{ delay:150,distance:"70px",duration:900,easing:'ease-in-out',origin:'bottom',scale:'0.85'})
+ScrollReveal().reveal('#trainerText h1',{ duration:3000,move:0})
+ScrollReveal().reveal('#trainerP2 img',{ duration:3000,move:0})
+ScrollReveal().reveal('#trainerP2 h1',{ delay:150,distance:"60px",duration:900,easing:'ease-in-out',origin:'bottom'})
+
+ScrollReveal().reveal('#parallex2 h1',{ delay:150,distance:"60px",duration:900,easing:'ease-in-out',origin:'left'})
+ScrollReveal().reveal('#parallex2 button',{ delay:150,distance:"60px",duration:900,easing:'ease-in-out',origin:'right'})
+
+ScrollReveal().reveal('#blog h1',{ delay:150,distance:"70px",duration:900,easing:'ease-in-out',origin:'top'})
+ScrollReveal().reveal('#blog button',{ delay:150,distance:"70px",duration:900,easing:'ease-in-out',origin:'bottom'})
+
+ScrollReveal().reveal('footer img ,footer button,footer h1,footer p,footer li',{ duration:3000,move:0})
+
+
+ 
+
+
+
+
